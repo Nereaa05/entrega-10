4ae604a393303bd9c5e19c4b0aad7b715e93b42f
Nereaa05 <130614479+Nereaa05@users.noreply.github.com>
2023-08-29 05:22:44 -0300

    Agregue js.

---
+++ tp10.js
@@ -0,0 +1,15 @@
+var nombre= "Nerea"; // String
+var apellido= "Espindola"; // String
+
+console.log(nombre + " " + apellido);
+
+
+var number1= 10;
+var number2= 15;
+var suma= number1 + number2;
+
+console.log(suma);
+
+var number3= suma + 15;
+
+console.log(number3);
